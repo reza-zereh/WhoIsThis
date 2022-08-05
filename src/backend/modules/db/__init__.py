f2f1c52330775387e6f944c2d97c6829aa168fce
ironcladgeek <ironcladgeek@hotmail.com>
2022-08-05 22:42:43 +0000

    [refactor]		Rename paths.DB_URL to paths.DB_CONN_STR

--- src/backend/modules/db/__init__.py
+++ src/backend/modules/db/__init__.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine, orm
 
 from libs import paths
 
-engine = create_engine(paths.DB_URL, echo=False)
+engine = create_engine(paths.DB_CONN_STR, echo=False)
 mapper_registry = orm.registry()
 Base = mapper_registry.generate_base()
