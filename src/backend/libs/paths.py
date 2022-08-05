1f147b8d22de1b86fb5b8e1f9b19c004b80583e2
ironcladgeek <ironcladgeek@hotmail.com>
2022-08-05 20:21:34 +0000

    [refactor]		Update paths

--- src/backend/libs/paths.py
+++ src/backend/libs/paths.py
@@ -3,7 +3,10 @@ from pathlib import Path
 SRC_DIR = Path(__file__).resolve().parent.parent.parent
 BACKEND_DIR = SRC_DIR / 'backend'
 APP_DIR = BACKEND_DIR / 'app'
-CORE_DIR = BACKEND_DIR / 'core'
-UTILS_DIR = BACKEND_DIR / 'utils'
+MODULES_DIR = BACKEND_DIR / 'modules'
+LIBS_DIR = BACKEND_DIR / 'libs'
 ASSETS_DIR = BACKEND_DIR / 'assets'
 MASKS_DIR = ASSETS_DIR / 'masks'
+STORAGE_DIR = BACKEND_DIR / 'storage'
+
+DB_URL = f"sqlite:///{STORAGE_DIR}/face.db"
