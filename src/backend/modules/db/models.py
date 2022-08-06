e508e8e0c5bc225b436ed5881bb90a7b38ba8d34
ironcladgeek <ironcladgeek@hotmail.com>
2022-08-06 23:10:01 +0300

    [feature]		Create DB model for 'models' table

--- src/backend/modules/db/models.py
+++ src/backend/modules/db/models.py
@@ -13,6 +13,15 @@ class Member(Base):
     photos = relationship("Photo", back_populates="member")
 
 
+class Model(Base):
+    __tablename__ = 'models'
+    # columns
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String)
+    # relationships
+    photos = relationship("Photo", back_populates="model")
+
+
 class Photo(Base):
     __tablename__ = 'photos'
     # columns
@@ -20,5 +29,7 @@ class Photo(Base):
     filename = Column(String)
     embedding = Column(String)
     member_id = Column(Integer, ForeignKey("members.id"))
+    model_id = Column(Integer, ForeignKey("models.id"))
     # relationships
     member = relationship("Member", back_populates="photos")
+    model = relationship("Model", back_populates="photos")
