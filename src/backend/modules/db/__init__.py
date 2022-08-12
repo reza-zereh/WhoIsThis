b4574f1cbf17581bba805bf928f75f2bb3c4e72f
ironcladgeek <ironcladgeek@hotmail.com>
2022-08-12 23:25:32 +0300

    [feature]	Define get_db_conn_str() for creating db connection string using os env vars

--- src/backend/modules/db/__init__.py
+++ src/backend/modules/db/__init__.py
@@ -1,7 +1,25 @@
+import os
 from sqlalchemy import create_engine, orm
 
-from libs import paths
+from libs import paths, utils
 
-engine = create_engine(paths.DB_CONN_STR, echo=False)
+utils.load_env()
+
+
+def get_db_conn_str():
+    driver = os.getenv('DB_CONNECTION')
+    host = os.getenv('DB_HOST')
+    port = os.getenv('DB_PORT')
+    dbname = os.getenv('DB_DATABASE')
+    username = os.getenv('DB_USERNAME')
+    password = os.getenv('DB_PASSWORD')
+
+    if driver == 'sqlite':
+        return f"sqlite:///{str(paths.STORAGE_DIR)}/{dbname}"
+
+    return f"{driver}://{username}:{password}@{host}:{port}/{dbname}"
+
+
+engine = create_engine(get_db_conn_str(), echo=False)
 mapper_registry = orm.registry()
 Base = mapper_registry.generate_base()
